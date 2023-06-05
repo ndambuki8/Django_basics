2ed6994bfc9657f842aa67d118c882380a7dc6bf
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-05 16:11:07 +0300

    Complete simple website

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+# CRUD - Create, Read/Retrieve, Update, Delete, 
+
+#1 - List all of the posts
+
+def post_list_view(request):
+    post_objects = Post.objects.all()
+
+    context = {
+        'post_objects':post_objects
+    }
+    return render(request, "posts/index.html", context)
